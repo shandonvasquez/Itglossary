b382b2e146e24a6874570356ddf8228db9306e44
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-03-05 08:21:04 -0600

    Proyect changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-const countapi = require('countapi-js');
-
-// or with ES6
-import countapi from 'countapi-js';
-
-countapi.visits().then((result) => {
-  console.log(result.value);
-});
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div></div>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 import React from 'react';
 
